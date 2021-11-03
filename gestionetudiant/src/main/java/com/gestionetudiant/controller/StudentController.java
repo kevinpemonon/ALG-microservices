c708043407b9bf6b3a9567367f95900225bf0043
PEMONON <kevin.pemonon@atos.net>
2021-11-03 13:39:50 +0100

    add project hotelManagement

--- gestionetudiant/src/main/java/com/gestionetudiant/controller/StudentController.java
+++ gestionetudiant/src/main/java/com/gestionetudiant/controller/StudentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gestionetudiant.dao.GestionStudent;
+//import com.gestionetudiant.dao.GestionStudent;
 import com.gestionetudiant.dao.StudentDao;
 import com.gestionetudiant.entity.Student;
 
---
+++ hotelManagement/hotelManagement/src/test/java/com/hotel/hotelManagement/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.hotelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
