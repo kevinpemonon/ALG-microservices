361fdb62c22d0d9218b436034a406f132a50ca0c
PEMONON <kevin.pemonon@atos.net>
2021-11-04 13:47:22 +0100

    add eureka

--- hotel_prof_phase_2/microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ hotel_prof_phase_2/microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -3,10 +3,11 @@ package com.mpaiement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MpaiementApplication {
 
